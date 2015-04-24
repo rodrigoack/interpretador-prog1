f1a26a4a725ff9c49fe42a22d46d1a06a9cea34e
guihermes <guilherme_hermes182@hotmail.com>
2015-04-23 21:50:33 -0300

    teste

--- README.md
+++
@@ -1,2 +0,0 @@
-# t800-command-protocol
-Linguagem de programação esotérica com comandos baseados em frases clássicas dos personagens de Arnold Schwartzenegger.
--- fonte/T800CP.java
+++ fonte/T800CP.java
@@ -8,24 +8,25 @@
  * Vanessa Sassi: <burnoutvanessa@gmail.com>
  */
 
+import java.util.Scanner;
+
 public class T800CP{
 	public static void main(String args[]){
 		Tokenizer tokenizer = new Tokenizer();
-
+        Scanner hue = new Scanner(System.in);
         /*TODO: Testar tokenizer com tokens de operações simples pra depois trocar pra nossa linguagem.*/
-        tokenizer.add("sin|cos|exp|ln|sqrt", 1);   //funções matemáticas
+        tokenizer.add("sin|cos|exp|ln|sqrt[^a-zA-Z0-9]?", 1);   //funções matemáticas
         tokenizer.add("\\(", 2);                   //abre parênteses
         tokenizer.add("\\)", 3);                   //fecha parênteses
         tokenizer.add("[+-]", 4);                  //soma ou subtração
         tokenizer.add("[*/]", 5);                  //multiplicação ou divisão
         tokenizer.add("\\^", 6);                   //elevado
         tokenizer.add("[0-9]+", 7);                //números inteiros
-        tokenizer.add("[a-zA-Z][a-zA-Z0-9_]*", 0); //variáveis 
+        tokenizer.add("[a-zA-Z][a-zA-Z0-9_]*", 8); //variáveis 
 
         try{
-            tokenizer.tokenize("cos");
-            tokenizer.tokenize(" sin(x) * (1 + var_12) + ln(3 / 2) ^ rodrgo");
-
+            tokenizer.tokenize(" sin(x) * (1 + var_12) + ln(3 / 2) ^ ");
+            tokenizer.tokenize(hue.nextLine());
             for(Tokenizer.Token tok : tokenizer.getTokens()){
                 System.out.println("" + tok.token + " " + tok.sequence);
             }
